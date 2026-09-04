4cbf02b8ddd108dd6d74c5bb371699e93a33e832
gmega <giuliano.mega@gmail.com>
2026-09-04 15:06:51 -0300

    improve docs

--- tools/harness/emu.bash
+++ tools/harness/emu.bash
@@ -25,33 +25,33 @@ emu_setup() {
   sudo ip netns add "${MIX_NS}"
 
   # Brings up the namespace's loopback interface (by default it's down),
-  # and sets the MTU to 1500:
+  # and sets the MTU to 1500 (by default it's 65536):
   sudo ip -n "${MIX_NS}" link set dev lo up mtu 1500
 
   # We want to apply netem to the libp2p connections, but we don't
   # want to apply it to API calls as that would make our lives miserable :-)
-  # We therefore create a two-class qdisc. For simplicity, we use PRIO
-  # with two bands: 1 for libp2p, 2 for everything else.
+  # We therefore create a two-class qdisc - one class gets netem, the other
+  # doesn't. We use PRIO (stateful qdisc) to implement that for simplicity.
 
-  # Sets prio as root qdisc with 2 classes, we also set the priomap to map
-  # all packets to band 1 (where we'll put netem) by default.
+  # Sets PRIO as root qdisc with 2 classes (bands 0 and 1). Band 0 will be
+  # unshaped, Band 1 will get netem. In the absence of filters, PRIO will classify
+  # packets based on the priomap, which maps the value of the IP TOS field to a band.
+  # We set a priomap that maps everything to band 1 (netem).
   _emu_tc qdisc add dev lo root handle 1:0 prio bands 2 \
     priomap 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1
 
-  # The first class (band 0) is unshaped, we'll use it for the APIs.
-  _emu_tc qdisc add dev lo parent 1:1 handle 10: pfifo
-
-  # Second class (band 1) gets netem.
-  _emu_tc qdisc add dev lo parent 1:2 handle 20: netem "${_netem_params[@]}"
-
-  # Finally, we add inbound and outbound filters to the API port so those don't go to netem.
-  # The reason we need both directions is because the API caller (e.g. curl) might use a
-  # different source port, but API_PORT will always be either in source or destination.
-  # Note that this assumes that no libp2p client will EVER use the API port as source (which)
-  # should hold true as it should be already occupied by the API itself, but things like SO_REUSEPORT
-  # could mess that up.
+  # As configured so far, every packet goes to netem. Here we add filters which tag packets
+  # coming from and to the API ports. We need to do both because clients (e.g. curl) may
+  # use random source ports, but the API port will always be in either source or destination.
   _emu_tc filter add dev lo parent 1:0 protocol ip u32 match ip sport "${TR_API_PORT}" 0xffff flowid 1:1
   _emu_tc filter add dev lo parent 1:0 protocol ip u32 match ip dport "${TR_API_PORT}" 0xffff flowid 1:1
+
+  # Finally, we set the qdiscs in each class:
+  # Band 0 (for the APIs) gets a simple pfifo.
+  _emu_tc qdisc add dev lo parent 1:1 handle 10: pfifo
+
+  # Band 1 (for libp2p) gets netem.
+  _emu_tc qdisc add dev lo parent 1:2 handle 20: netem "${_netem_params[@]}"
 }
 
 ## Enters the emulation environment. Can be used both from an interactive shell or as a command.
@@ -60,9 +60,16 @@ emu_setup() {
 emu_enter() {
   if emu_is_inside; then
     echoerr "Inside $MIX_NS namespace."
-    return
+    return 0
   fi
 
+  if [[ ${#_netem_params[@]} -eq 0 ]]; then
+    echoerr "No netem parameters set. Please call emu_set_params"
+    echoerr "before calling emu_enter."
+    return 1
+  fi
+
+  # TODO implement this rootless
   echoerr "Entering emulation environment."
   echoerr "WARNING: this requires 'sudo'"
 
