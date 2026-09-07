f9224b2ef52d5699392ebbe4f5fa769f774fb80e
gmega <giuliano.mega@gmail.com>
2026-09-07 14:44:16 -0300

    modify profiles, generalize analysis

--- tools/harness/emu-profiles.bash
+++ tools/harness/emu-profiles.bash
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
-# TODO implement informed profiles (i.e. research some data)
+# TODO implement informed profiles (i.e. research some data). Note that
+#   capped profiles apply network-wide, i.e., the bandwidth is split across
+#   the whole network.
 declare -gA EMU_PROFILES=(
-  [wired]="delay 10ms 2ms distribution normal loss 0.1% rate 100mbit"
-  [wired-slow]="delay 10ms 2ms distribution normal loss 0.1% rate 1mbit"
-  [wired-lossy]="delay 10ms 2ms distribution normal loss 1% rate 100mbit"
-  [hi-delay-jittery]="delay 100ms 30ms distribution normal loss 0.1% rate 100mbit"
-  [hi-delay-jittery-lossy]="delay 100ms 30ms distribution normal loss 1% rate 100mbit"
+  [wired]="delay 10ms 2ms distribution normal loss 0.1%"
+  [wired-lossy]="delay 10ms 2ms distribution normal loss 1%"
+  [wired-capped]="delay 10ms 2ms distribution normal loss 0.1% rate 100mbit"
+  [wired-very-capped]="delay 10ms 2ms distribution normal loss 0.1% rate 1mbit"
+  [hi-delay-jittery]="delay 100ms 30ms distribution normal loss 0.1%"
+  [hi-delay-jittery-lossy]="delay 100ms 30ms distribution normal loss 1%"
 )
 
 export EMU_PROFILES
